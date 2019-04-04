30e1b647365220b049b46d4184e4084aaa4d2620
ravelsirrom <45865526+ravelsirrom@users.noreply.github.com>
2019-04-04 02:15:10 -0400

    updated js
    added user input browser side and ability to remove current and future items added to list

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+$( document ).ready(function() {
+    // Handler for .ready() called.
+    console.log("ready")
+    $("#js-shopping-list-form").on("submit", function(event) { 
+    event.preventDefault()
+    const userInput = $(this).find('#shopping-list-entry');
+    $("#shopping-list-entry").val();
+    
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+      $(this).closest('li').remove();
+    });
+    console.log("submit")
+    console.log($("#shopping-list-entry").val())
+    })
+  });
