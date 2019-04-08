b14eed945b7fc4c75e137e088cd987ab9884a473
ravelsirrom <45865526+ravelsirrom@users.noreply.github.com>
2019-04-08 10:08:00 -0400

    updated js

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ $( document ).ready(function() {
     const userInput = $(this).find('#shopping-list-entry');
     $("#shopping-list-entry").val();
     
-    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-      $(this).closest('li').remove();
-    });
-    console.log("submit")
-    console.log($("#shopping-list-entry").val())
-    })
+    $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
   });
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+
+});
